c5ddbbed9cb02a52984dac55040432dc309dcc1e
lklll1212 <lklll1212@gmail.com>
2024-09-11 19:51:04 +0900

    레벨 2-5 : 테스트 연습 - 1 matches 메서드 () 안에 값은 비밀번호, 암호화된 비밀번호 순으로 되어야 하기 때문에 수정.

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -14,15 +14,16 @@ class PasswordEncoderTest {
     private PasswordEncoder passwordEncoder;
 
     @Test
-    void matches_메서드가_정상적으로_동작한다() {
+    void matches() {
         // given
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // when 비밀번호 비교는 passwordEncoder.matches(비밀번호, 암호화된 비밀번호) 를 이용해야합니다.
+        // matches 는 암호화된 password 에서 salt 값을 추출, 원본에 salt 값을 이용해 해싱, 그 값으로 비교.
+        boolean matches = passwordEncoder.matches(rawPassword,encodedPassword);
 
-        // then
+        // assertTrue 안에 값이 True 이면 예상이 true 값도 true 문제 없음.
         assertTrue(matches);
     }
 }
